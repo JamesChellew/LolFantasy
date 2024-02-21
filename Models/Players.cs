a2468af74c7f83858b765a54ba4d1533618be3ea
JAMES CHELLEW <30071566@tafe.wa.edu.au>
2024-02-21 17:25:18 +0800

    Save Point Before Foreign Key Implementation

--- Models/Players.cs
+++ Models/Players.cs
@@ -1,6 +1,20 @@
-﻿namespace LolFantasy.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LolFantasy.Models
 {
     public class Players
     {
+        [Key] 
+        public int PlayerId { get; set; }
+        [Required]
+        private string InGameName;
+        private string FullName;
+        private string Role;
+        private int Kills;
+        private int Deaths;
+        private int Assists;
+        private int CreepScore;
+        private DateTime CreatedTime;
+        private DateTime UpdatedTime;
     }
 }
---
+++ Models/Team.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LolFantasy.Models
+{
+    public class Team
+    {
+        [Key]
+        private int TeamId { get; set; }
+        [Required]
+        private string TeamName { get; set; }
+        private List<Players> Players { get; set; }
+        private DateTime CreatedTime;
+        private DateTime UpdatedTime;
+    }
+}
