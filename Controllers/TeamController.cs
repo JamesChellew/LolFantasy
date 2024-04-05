786d56beda98fc06450c09b2db33f47277fd3c89
James Chellew <JamesPChellew@outlook.com>
2024-04-05 12:40:19 +0800

    Added TeamController enpoints and database interactions

--- Controllers/TeamController.cs
+++ Controllers/TeamController.cs
@@ -1,83 +1,104 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using LolFantasy.Data;
+using LolFantasy.Models;
+using LolFantasy.Models.Dto;
 using Microsoft.AspNetCore.Mvc;
 
 namespace LolFantasy.Controllers
 {
+    [Route("api/TeamAPI")]
+    [ApiController]
     public class TeamController : Controller
     {
-        // GET: TeamController
-        public ActionResult Index()
+        private readonly ApplicationDbContext _db;
+
+        public TeamController(ApplicationDbContext db)
         {
-            return View();
+            _db = db;
         }
 
-        // GET: TeamController/Details/5
-        public ActionResult Details(int id)
+        // GET: api/TeamAPI/GetTeams
+        [HttpGet(Name = "GetTeams")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public ActionResult<IEnumerable<Team>> GetAllTeams()
         {
-            return View();
-        }
-
-        // GET: TeamController/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: TeamController/Create
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
-        {
-            try
+            var allTeams = _db.Teams.ToList();
+            var allTeamsDto = new List<TeamDto>();
+            foreach (Team t in allTeams)
             {
-                return RedirectToAction(nameof(Index));
+                allTeamsDto.Add(t.ConvertToTeamDto());
             }
-            catch
-            {
-                return View();
-            }
+            return Ok(allTeamsDto);
         }
 
-        // GET: TeamController/Edit/5
-        public ActionResult Edit(int id)
+        // GET: api/TeamAPI/GetTeam/{int}
+        [HttpGet("{int}", Name = "GetTeam")]
+        public ActionResult<IEnumerable<TeamDto>> GetTeam(int teamId)
         {
-            return View();
+            if (teamId < 0)
+            {
+                return BadRequest();
+            }
+            Team team = _db.Teams.FirstOrDefault(t => t.TeamId == teamId);
+            if (team == null)
+            {
+                return NotFound();
+            }
+            return Ok(team);
         }
 
-        // POST: TeamController/Edit/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
+        // POST: api/TeamAPI/CreateTeam
+        [HttpPost(Name = "CreateTeam")]
+        public ActionResult CreateTeam(TeamDto teamDto)
         {
-            try
+            if (teamDto == null)
             {
-                return RedirectToAction(nameof(Index));
+                return BadRequest();
             }
-            catch
+            if (teamDto.TeamId > 0)
             {
-                return View();
+                return StatusCode(StatusCodes.Status500InternalServerError);
             }
+            Team team = teamDto.ConvertToTeam();
+            _db.Teams.Add(team);
+            _db.SaveChanges();
+            return Ok();
         }
 
-        // GET: TeamController/Delete/5
-        public ActionResult Delete(int id)
+        // PUT: api/TeamAPI/UpdateTeam/{int}
+        [HttpPut("{int}", Name = "UpdateTeam")]
+        public ActionResult UpdateTeam(int teamId, TeamDto teamDto)
         {
-            return View();
+            if (teamDto == null || teamId != teamDto.TeamId)
+            {
+                return BadRequest();
+            }
+            var teamToBeUpdated = _db.Teams.FirstOrDefault(t => t.TeamId == teamId);
+            if (teamToBeUpdated == null)
+            {
+                return NotFound(teamId);
+            }
+            var team = teamDto.ConvertToTeam();
+            _db.Teams.Update(team); // _db remembers the index from FirstOrDefault and updates that record.
+            _db.SaveChanges();
+            return Ok();
         }
 
-        // POST: TeamController/Delete/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
+        // DELETE: api/TeamAPI/DeleteTeam/{int}
+        [HttpDelete("{int}", Name = "DeleteTeam")]
+        public ActionResult Delete(int teamId)
         {
-            try
+            if (teamId > 0)
             {
-                return RedirectToAction(nameof(Index));
+                return BadRequest();
             }
-            catch
+            var team = _db.Teams.FirstOrDefault(t => t.TeamId == teamId);
+            if (team == null)
             {
-                return View();
+                return NotFound(teamId);
             }
+            _db.Teams.Remove(team);
+            _db.SaveChanges();
+            return Ok();
         }
     }
 }
